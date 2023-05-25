ac538933ad0ff2c2dc4c256e761487e5d355476d
tomas0011 <tomasbenjamin117@gmail.com>
2023-05-25 17:09:12 -0300

    js para temporizador

---
+++ index.js
@@ -0,0 +1,13 @@
+let temporizador = document.querySelector("#temporizador");
+
+const fechaDeCumple = new Date('2023-06-11T01:00:00.000Z')
+
+setInterval(() => {
+  const fechaActual = new Date();
+  const diferencia = fechaDeCumple - fechaActual;
+  var segundos = Math.floor(diferencia / 1000) % 60;
+  var minutos = Math.floor(diferencia / (1000 * 60)) % 60;
+  var horas = Math.floor(diferencia / (1000 * 60 * 60)) % 24;
+  var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  temporizador.innerText = `${dias} Dias ${horas} Horas ${minutos} Minutos ${segundos} Segundos`
+}, 1000)
