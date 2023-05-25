2707f28d994378a143ace10f71f1dab0f7d81718
Tomas Benjamin Vasquez <58223692+tomas0011@users.noreply.github.com>
2023-05-25 17:54:18 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+    <div>
+      <header>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"/>
       </header>
+      <nav></nav>
+      <main>
+        <div class="tarjeta">
+          Mis 50 Andrea
+        </div>
+        <div class="clock">
+          <img src="https://picsum.photos/50/50" alt="reloj"/>
+          <h2>3 Dias 4 Horas 25 Minutos</h2>
+        </div>
+        <div class="tarjeta">
+          <div>
+            <img src="https://picsum.photos/50/50" alt="traje"/>
+            <img src="https://picsum.photos/50/50" alt="vestido"/>
+          </div>
+          <h2>Traer algo blanco</h2>
+        </div>
+        <div class="tarjeta">
+          <img src="https://picsum.photos/50/50" alt="trompeta"/>
+          Es una fiesta sorpresa, por lo que procuren llegar a horario o antes
+        </div>
+        <div class="tarjeta">
+          <ul>
+            <li>
+              <img src="https://picsum.photos/50/50" alt="ubicacion"/>
+              jufre 452
+            </li>
+            <li>
+              <img src="https://picsum.photos/50/50" alt="calendario"/>
+              5/6/23
+            </li>
+          </ul>
+          <img src="https://picsum.photos/300/300" alt="mapa"/>
+        </div>
+        <div class="tarjeta">
+          <img src="https://picsum.photos/50/50" alt="whatsapp"/>
+          <h2>Confirmar asistencia</h2>
+        </div>
+      </main>
+      <footer></footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </div>
   );
 }
