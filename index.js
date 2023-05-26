dfd2155bfc4ff1b989bf6d7edcbd6a3b83d28869
tomas0011 <tomasbenjamin117@gmail.com>
2023-05-26 16:46:55 -0300

    stype

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const fechaDeCumple = new Date('2023-06-11T01:00:00.000Z')
 setInterval(() => {
   const fechaActual = new Date();
   const diferencia = fechaDeCumple - fechaActual;
-  var segundos = Math.floor(diferencia / 1000) % 60;
   var minutos = Math.floor(diferencia / (1000 * 60)) % 60;
   var horas = Math.floor(diferencia / (1000 * 60 * 60)) % 24;
   var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-  temporizador.innerText = `${dias} Dias ${horas} Horas ${minutos} Minutos ${segundos} Segundos`
+  temporizador.innerText = `${dias} Dias ${horas} Horas ${minutos} Minutos`
 }, 1000)
